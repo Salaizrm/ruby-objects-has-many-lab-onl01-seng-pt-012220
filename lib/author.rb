b2044d59fd94b1ec71074020b88ae479139fd40d
Robert Salaiz <work.salaizrm@gmail.com>
2020-02-17 20:29:15 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,6 +2,8 @@ class Author
   
   attr_accessor :name, :posts
   
+  post_count = 0 
+  
   def initialize (name)
     @name = name
   end
@@ -9,5 +11,19 @@ class Author
   def posts
     Post.all.select {|posts| posts}
   end
+  
+  def add_post (posts)
+    song.artist = self
+  end
+  
+  def add_post_by_title (title_name)
+    author = Post.new(title_name)
+    add_song(author)
+  end
+  
+  def self.post_count
+    Post.all.count
+  end
+
 
 end
